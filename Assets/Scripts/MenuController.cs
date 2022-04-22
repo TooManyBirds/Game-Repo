027fe6ebe56cc8a6eb191734b1ed393bddedf30e
Jacob <jdank99@gmail.com>
2022-04-22 16:28:00 -0700

    Added UI Scenes and Hub Scene
    pushing p respectfully

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+
+    public string MainMenu;
+    public string StartLevel;
+    public string Options;
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(StartLevel);
+    }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene(MainMenu);
+    }
+
+    public void options()
+    {
+        SceneManager.LoadScene(Options);
+    }
+
+
+}
