ae95c1b82264b39f8c60b73d6a54a03106fb4865
TooManyBirds <Frankieiscasillas@gmail.com>
2022-04-22 21:51:33 -0700

    Make PlayerController work
    add double jump and horizontal movement(needs camera script with cinemachine)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,71 +6,95 @@ public class PlayerController : MonoBehaviour
 {
     //Components
     public CharacterController cont;
+    public Transform camera;
 
     // Movement Variables
     [SerializeField]
-    private Vector3 playerVel;
-    private bool groundedPlayer;
-    private float playerSpd;
-    private float jumpHeight;
-    private float gravity = -9.81f;
+    public float playerSpeed = 6f;
+    public float jumpForce = 20f;
+    public float jumpCount = 0f;
+   
+    float turnVelocity;   
+    public float turnSmoothing = 0.1f;
+   
+    //Gravity and stuff
+    public float gravity = 20f;
+    bool grounded;
+    Vector3 velocity;
 
-    [Space]
-    private float horizontalMove;
-    private float direction = 1;
+    //Ground Checking stuff
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
 
     void Start()
     {
-        cont = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 
     void Update()
     {
-        // Movement
-        groundedPlayer = cont.isGrounded;
-        if(groundedPlayer && playerVel.y < 0)
+        // Gravity and Jumping
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (grounded && velocity.y < 0)
         {
-            playerVel.y = 0;
+            velocity.y = 0;
+            jumpCount = 0;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        cont.Move(move * Time.deltaTime * playerSpd);
-
-        if (move != Vector3.zero)
+        if (Input.GetButtonDown("Jump") && jumpCount < 2)
         {
-            gameObject.transform.forward = move;
+            velocity.y = jumpForce;
+            jumpCount++;
         }
+        // Gravity
+        velocity.y += gravity * Time.deltaTime;
 
-        if(Input.GetButtonDown("Jump") && groundedPlayer)
+        if (velocity.y < -20f)
         {
-            playerVel.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+            velocity.y = -20f;
         }
+        //Changing player velocity
+        cont.Move(velocity * Time.deltaTime);
 
-        // Contstantly pushed the player down
-        playerVel.y += gravity * Time.deltaTime;
-        cont.Move(playerVel * Time.deltaTime);
+        //Horizontal player movement
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-
-
-        //Animation Updater
-        horizontalMove = Input.GetAxisRaw("Horizontal");
-        if(horizontalMove != 0)
-        { 
-            //Set walk/run animation
-        }
-        else
+        if (direction.magnitude >= 0.1f)
         {
-            //Undo animation
+            // Playerboy point in movement direction
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnSmoothing);
+           
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            cont.Move(moveDir.normalized * playerSpeed * Time.deltaTime);
         }
-        if(horizontalMove > 0)
-        {
-            direction = 1;
-        }
-        if(horizontalMove < 0)
-        {
-            direction = -1;
-        }
-
     }
 }
+
+
+
+//        //Animation Updater
+//        horizontalMove = Input.GetAxisRaw("Horizontal");
+//        if(horizontalMove != 0)
+//        { 
+//            //Set walk/run animation
+//        }
+//        else
+//        {
+//            //Undo animation
+//        }
+//        if(horizontalMove > 0)
+//        {
+//            direction = 1;
+//        }
+//        if(horizontalMove < 0)
+//        {
+//            direction = -1;
+//        }
+
+//    }
+//}
